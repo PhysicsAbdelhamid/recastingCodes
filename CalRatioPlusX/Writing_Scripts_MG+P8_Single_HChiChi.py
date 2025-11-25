54f93593da875d4026c03901daaa42ec17409c5e
Haddad Abdelhamid <abdelhamid.haddad@cern.ch>
2025-11-25 19:37:29 +0100

    solving baryogenesis bug

--- CalRatioPlusX/Writing_Scripts_MG+P8_Single_HChiChi.py
+++ CalRatioPlusX/Writing_Scripts_MG+P8_Single_HChiChi.py
@@ -31,15 +31,15 @@ f.write(f"launch Script_{slug} \n")
 f.write(f"shower=Pythia8 \n")  # Add Pythia
 f.write(f"0 \n")  # Launch the computation
 f.write(f"set nevents = {nevent} \n")  # Generate 10% extra events?
-f.write(f"set Mxchi {mChi} \n")  # Set mass of chi
+f.write(f"set Mxchi {mass_Chi} \n")  # Set mass of chi
 f.write(f"set epsilon 1e-10 \n") # Set the couplings for epsilon.
 f.write(f"set kap 1e-4 \n") # Set the couplings for kappa.
 f.write(f"set time_of_flight 0 \n" ) # Set the time of flight of the particle.
 f.write(f"set event_norm = sum \n")
 f.write(f"set lhe_version = 3.0\n" )
 f.write(f"set cut_decays = F \n")
-f.write(f"set pdlabel = pdflabel \n")  # Disable the cuts.
-f.write(f"set lhaid = lhaid \n")  
+f.write(f"set pdlabel = lhapdf \n")  # Disable the cuts.
+f.write(f"set lhaid = 315000 \n")  # NNPDF31_lo_as_0118
   
 f.write(f"set ptj = 0 \n")
 f.write(f"set ptb = 0 \n")
